6d8b570183f36860cdb60b64c53bd46b9af03ee1
ELDEVODE01~ <elpraise20@gmail.com>
2022-05-05 22:24:10 +0100

    Home component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <h2>Let's Get rich baby!😉</h2>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route
+            exact
+            path="/home"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import Header from './Header';
+
+const Home = (props) => {
+  return (
+    <>
+      <div>
+          Home
+      </div>
+    </>
+  );
+};
+
+export default Home;
